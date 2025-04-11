452c187a1805138796c0a326aaf5529e2bd09ac1
Dsolm <dsola2405@gmail.com>
2025-04-11 16:36:28 +0200

    Respuesta

---
+++ respuestas.py
@@ -0,0 +1,15 @@
+# 1. Quina de les següents propietats es pot trobar dins de la classe PATH?
+
+## Connexions entre les estacions del mapa
+
+# 2. Donades les coordenades [181, 197], quina és l'estació/estacions de metro més properes?
+
+## ['Poblenou L3']
+
+# 3. Quins camins obtenim quan cridem la funció remove_cycles sobre els camins: [[7, 6, 1, 8, 5], [7, 6, 1, 8, 2], [7, 6, 1, 8, 7], [7, 6, 1, 8, 6], [7, 6, 1, 8, 4]].
+
+## [[7, 6, 1, 8, 5], [7, 6, 1, 8, 2], [7, 6, 1, 8, 4]]
+
+# 4.Quin camí obtenim quan utilitzem el mètode de CERCA EN AMPLADA des de l'estació Sagrera L2 per arribar a Navas L1
+
+## [7,1,2]
